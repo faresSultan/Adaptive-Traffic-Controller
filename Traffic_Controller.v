ebfce6f6eb68bf30fe88e3c6b15441e96cbdeed5
faresSultan <fares.sultan9@gmail.com>
2024-12-01 22:14:50 +0200

    Next state logic

--- Traffic_Controller.v
+++ Traffic_Controller.v
@@ -18,9 +18,6 @@ module Traffic_Controller (Sa,Sb,Sc,Sd,clk,rst_n,counter_value,Ta,Tb,Tc,Td,load_
     reg [2:0] current_state, next_state;
 
     //---
-//Counter (clk,load_data,load,counter)
-    Counter internal_counter (.clk(clk),.load_data(),.load(load_counter),.counter(counter_value));
-
 
     // current state logic
 
@@ -40,14 +37,81 @@ module Traffic_Controller (Sa,Sb,Sc,Sd,clk,rst_n,counter_value,Ta,Tb,Tc,Td,load_
                 if (((Sa>Sb)&&(Sa>Sc)&&(Sa>Sd)) || counter_value !=0) begin // A is the highest priority or counter isn't finished
                     next_state <= Ga;      // remain in the same state
                 end
-            end
-            Gb: 
-            Gc: 
-            Gd: 
-            Oa: 
-            Ob: 
-            Oc: 
-            Od: 
+                else next_state <= Oa;    // move to orange state in preparation to stop the traffic of this side
+                end
+
+            Gb: begin
+                if (((Sb>Sa)&&(Sb>Sc)&&(Sb>Sd)) || counter_value !=0) begin // B is the highest priority or counter isn't finished
+                    next_state <= Gb;      // remain in the same state
+                end
+                else next_state <= Ob;    // move to orange state in preparation to stop the traffic of this side
+                end
+
+            Gc: begin
+                if (((Sc>Sa)&&(Sc>Sb)&&(Sc>Sd)) || counter_value !=0) begin // C is the highest priority or counter isn't finished
+                    next_state <= Gc;      // remain in the same state
+                end
+                else next_state <= Oc;    // move to orange state in preparation to stop the traffic of this side
+                end
+
+            Gd: begin
+                if (((Sd>Sa)&&(Sd>Sb)&&(Sd>Sc)) || counter_value !=0) begin // D is the highest priority or counter isn't finished
+                    next_state <= Gd;      // remain in the same state
+                end
+                else next_state <= Od;    // move to orange state in preparation to stop the traffic of this side
+                end
+
+            Oa: begin
+                    if (counter_value !=0)  // remaining in the same state untill counter ends
+                         next_state <= Oa;
+                    else if (((Sa == Sb)&&(Sa == Sc)&&(Sa == Sd)) || (Sb>Sa)) // moving to Gb if all traffics have equal prioroties or B is the highest
+                        next_state <= Gb;
+                    else if ((Sc>Sa)&&(Sc>Sb))   // moving to Gc if c has higher priority than A,B
+                        next_state <= Gc;
+                    else if ((Sd>Sa)&&(Sd>Sb)&&(Sd>Sc))  // D is the highest priority
+                        next_state <= Gd;
+                    else  // if any ubnormal case happened -> regular flow
+                        next_state<= Gb;  
+                end  
+
+            Ob: begin
+                    if (counter_value !=0)  // remaining in the same state untill counter ends
+                         next_state <= Ob;
+                    else if (((Sa == Sb)&&(Sa == Sc)&&(Sa == Sd)) || (Sc>Sb)) // moving to Gc if all traffics have equal prioroties or B is the highest
+                        next_state <= Gc;
+                    else if ((Sd>Sb)&&(Sd>Sc))   // moving to Gd if d has higher priority than B,C
+                        next_state <= Gd;
+                    else if ((Sa>Sb)&&(Sa>Sc)&&(Sa>Sd))  // A is the highest priority
+                        next_state <= Ga;
+                    else  // if any ubnormal case happened -> regular flow
+                        next_state<= Gc;  
+                end
+
+            Oc: begin
+                    if (counter_value !=0)  // remaining in the same state untill counter ends
+                         next_state <= Oc;
+                    else if (((Sa == Sb)&&(Sa == Sc)&&(Sa == Sd)) || (Sd>Sc)) // moving to Gd if all traffics have equal prioroties or B is the highest
+                        next_state <= Gd;
+                    else if ((Sa>Sc)&&(Sa>Sd))   // moving to Ga if a has higher priority than C,D
+                        next_state <= Ga;
+                    else if ((Sb>Sc)&&(Sb>Sd)&&(Sb>Sa))  // B is the highest priority
+                        next_state <= Gb;
+                    else  // if any ubnormal case happened -> regular flow
+                        next_state<= Gd;  
+                end
+
+            Od: begin
+                    if (counter_value !=0)  // remaining in the same state untill counter ends
+                         next_state <= Od;
+                    else if (((Sa == Sb)&&(Sa == Sc)&&(Sa == Sd)) || (Sa>Sd)) // moving to Ga if all traffics have equal prioroties or B is the highest
+                        next_state <= Ga;
+                    else if ((Sb>Sd)&&(Sb>Sa))   // moving to Gb if a has higher priority than D,A
+                        next_state <= Gb;
+                    else if ((Sc>Sd)&&(Sc>Sb)&&(Sc>Sa))  // c is the highest priority
+                        next_state <= Gc;
+                    else  // if any ubnormal case happened -> regular flow
+                        next_state<= Ga;  
+                end
             default: 
         endcase
     end
--- counter.v
+++ counter.v
@@ -1,18 +1,14 @@
-module Counter (clk,load_data,load,counter);
+module counter(clk,load,data,count);
 
-    input clk,load;
-    input [4:0] load_data;  // 5-bit counter to count from 1 - 30
-    output reg [4:0] counter;
-
-    reg [4:0] counter_value;
-    always @(posedge clk) begin
-        if (load) begin
-            counter_value <= load_data; 
-        end
-        else begin
-            counter <= counter_value-1;
-        end
-        
-    end
-    
-endmodule
+  input clk,load;
+  input [4:0] data;
+  output reg [4:0] count;
+  
+  always@(posedge clk) 
+  begin
+    if(load)    
+      count <= data;
+    else           
+      count <= count - 1;
+  end
+endmodule 
