c3a1e6af0644d5e5cc430136965b8abe296d66de
faresSultan <fares.sultan9@gmail.com>
2024-12-02 11:12:14 +0200

    .

--- Topmodule.v
+++ Topmodule.v
@@ -12,9 +12,19 @@ module Topmodule (
     wire [4:0] load_value;       // value to be loaded to the counter
     wire load_en;                // load enable
 
-    Traffic_Controller FSM (.Sa(Sa),.Sb(Sb),.Sc(Sc),.Sd(Sd),.clk(clk),.rst_n(rst_n),
-    .counter_value(counter_output),.Ta(Ta),.Tb(Tb),.Tc(Tc),.Td(Td),.load_counter(load_en),.load_value(load_value));
+    wire [1:0] Sa_reading;
+    wire [1:0] Sb_reading;
+    wire [1:0] Sc_reading;
+    wire [1:0] Sd_reading;
+
+    Traffic_Controller FSM (.Sa(Sa_reading),.Sb(Sb_reading),.Sc(Sc_reading),.Sd(Sd_reading),.clk(clk),.rst_n(rst_n)
+    ,.counter_value(counter_output),.Ta(Ta),.Tb(Tb),.Tc(Tc),.Td(Td),.load_counter(load_en),.load_value(load_value));
 
     counter internal_counter (.clk(clk),.load(load_en),.data(load_value),.count(counter_output));
-     
+
+    assign Sa_reading = Sa[0] + Sa[1];
+    assign Sb_reading = Sb[0] + Sb[1];
+    assign Sc_reading = Sc[0] + Sc[1];
+    assign Sd_reading = Sd[0] + Sd[1];
+ 
 endmodule
