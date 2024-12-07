644541cdd44181a10cd3c8180793abd9eb3ea0de
faresSultan <fares.sultan9@gmail.com>
2024-12-07 20:10:31 +0200

    adding rst

--- counter.v
+++ counter.v
@@ -1,14 +1,20 @@
-module counter(clk,load,data,count);
+module counter(clk,rst_n,load,data,count);
 
-  input clk,load;
+  input clk,load,rst_n;
   input [4:0] data;
   output reg [4:0] count;
   
-  always@(posedge clk) 
+  always@(posedge clk,negedge rst_n) 
   begin
-    if(load)    
+    if (!rst_n) begin
+      count<=30;
+    end
+    else begin
+      if(load)    
       count <= data;
     else           
       count <= count - 1;
+    end
+    
   end
 endmodule 
